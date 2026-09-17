07badbb239ec47fff4830500676e6926035791f2
EnoMor1399 <morganenoch1@gmail.com>
2026-09-17 14:06:46 +0000

    Integrate objective-site presentation with existing Protocol authority

--- ShadowProtocol-v1.0/Source/ShadowProtocol/Private/SPObjectiveSite.cpp
+++ ShadowProtocol-v1.0/Source/ShadowProtocol/Private/SPObjectiveSite.cpp
@@ -1,8 +1,34 @@
 #include "SPObjectiveSite.h"
 #include "Net/UnrealNetwork.h"
-ASPObjectiveSite::ASPObjectiveSite(){bReplicates=true;SetReplicateMovement(false);}
-void ASPObjectiveSite::SetActiveSite(bool bActive){if(HasAuthority())bActiveThisRound=bActive;}
+
+ASPObjectiveSite::ASPObjectiveSite()
+{
+    bReplicates = true;
+    SetReplicateMovement(false);
+    DisplayName = FText::FromString(TEXT("Archive Core"));
+}
+
+void ASPObjectiveSite::SetActiveSite(bool bActive)
+{
+    if (!HasAuthority())
+    {
+        return;
+    }
+
+    bActiveThisRound = bActive;
+    BP_OnObjectiveSiteStateChanged(bActiveThisRound);
+}
+
+void ASPObjectiveSite::OnRep_ActiveSite()
+{
+    BP_OnObjectiveSiteStateChanged(bActiveThisRound);
+}
+
 void ASPObjectiveSite::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
-    Super::GetLifetimeReplicatedProps(OutLifetimeProps);DOREPLIFETIME(ASPObjectiveSite,bActiveThisRound);
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+    DOREPLIFETIME(ASPObjectiveSite, SiteId);
+    DOREPLIFETIME(ASPObjectiveSite, DisplayName);
+    DOREPLIFETIME(ASPObjectiveSite, EmbassyZone);
+    DOREPLIFETIME(ASPObjectiveSite, bActiveThisRound);
 }
