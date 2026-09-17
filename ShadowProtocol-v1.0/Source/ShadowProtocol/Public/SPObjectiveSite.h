fa0464f669cd752c83767cb8a011c74d3831c946
EnoMor1399 <morganenoch1@gmail.com>
2026-09-17 14:06:37 +0000

    Enhance existing objective-site authority for v1.0

--- ShadowProtocol-v1.0/Source/ShadowProtocol/Public/SPObjectiveSite.h
+++ ShadowProtocol-v1.0/Source/ShadowProtocol/Public/SPObjectiveSite.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "SPObjectiveSite.generated.h"
@@ -7,11 +8,31 @@ UCLASS()
 class SHADOWPROTOCOL_API ASPObjectiveSite : public AActor
 {
     GENERATED_BODY()
+
 public:
     ASPObjectiveSite();
-    UPROPERTY(EditAnywhere, BlueprintReadOnly) FName SiteId = "ARCHIVE_A";
-    UPROPERTY(EditAnywhere, BlueprintReadOnly) FText DisplayName;
-    UPROPERTY(Replicated, BlueprintReadOnly) bool bActiveThisRound = false;
+
+    UPROPERTY(EditAnywhere, Replicated, BlueprintReadOnly, Category="Objective")
+    FName SiteId = TEXT("ARCHIVE-A");
+
+    UPROPERTY(EditAnywhere, Replicated, BlueprintReadOnly, Category="Objective")
+    FText DisplayName;
+
+    UPROPERTY(EditAnywhere, Replicated, BlueprintReadOnly, Category="Objective")
+    FName EmbassyZone = TEXT("ARCHIVE CORE");
+
+    UPROPERTY(ReplicatedUsing=OnRep_ActiveSite, BlueprintReadOnly, Category="Objective")
+    bool bActiveThisRound = false;
+
+    UFUNCTION(BlueprintCallable, Category="Objective")
     void SetActiveSite(bool bActive);
+
+    UFUNCTION(BlueprintImplementableEvent, Category="Objective")
+    void BP_OnObjectiveSiteStateChanged(bool bNowActive);
+
     virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+protected:
+    UFUNCTION()
+    void OnRep_ActiveSite();
 };
